f16539e3c9f68b4bb20880c1ceb99601ea43cae6
Satya <satyanandam.komera@gmail.com>
2026-03-15 12:25:18 +0530

    Refractor the Configmanager class for reading property files.

--- src/test/java/com/utility/ConfigManager.java
+++ src/test/java/com/utility/ConfigManager.java
@@ -1,23 +1,40 @@
 package com.utility;
 
 import java.io.File;
-import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class ConfigManager {
 
 	private static Properties prop = new Properties();
 
+	private static String filepath = "";
+
 	private ConfigManager() {
 	};
 
 	static {
-		File propfile = new File(
-				System.getProperty("user.dir") + File.separator+"src"+File.separator+"test"+File.separator+"resources"+File.separator+"com.properties"+File.separator+"config.qa.properties");
+
+		String environement = System.getProperty("env", "qa");
+		String environment = environement.toLowerCase().trim();
+
+		switch (environment) {
+
+		case "qa" -> filepath = "com.properties" + File.separator + "config.qa.properties";
+		case "dev" -> filepath = "com.properties" + File.separator + "config.dev.properties";
+		default -> filepath = "com.properties" + File.separator + "config.qa.properties";
+
+		}
+
+		InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(filepath);
+
+		if (is == null)
+			throw new RuntimeException("Can't find file at path");
+
 		try {
-			FileReader fr = new FileReader(propfile);
-			prop.load(fr);
+			prop.load(is);
+
 		} catch (IOException e) {
 
 			e.printStackTrace();
